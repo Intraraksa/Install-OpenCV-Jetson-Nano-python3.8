26bfdd6b761c769f2323842abb296f64d0444a7e
Natdanai Intraraksa <38306547+Intraraksa@users.noreply.github.com>
2023-09-23 10:51:43 +0700

    Update OpenCV-4-8-0.sh

--- OpenCV-4-8-0.sh
+++ OpenCV-4-8-0.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev
 sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev libpng-dev libtiff-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libglew-dev
 sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
-sudo apt-get install -y python3.8-dev python3.8-numpy python3.8-pip
+sudo apt-get install -y python3-dev python3-numpy python3-pip
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
 sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils qv4l2 
